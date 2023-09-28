e953f36d1a2a485c109b382dc8852f19911337a6
SHIYOU-ASUS <shuushuutan@gmail.com>
2023-09-28 15:18:13 +0900

    更改切換葉面用的iframe功能之檔案名並套用至index中

---
+++ src/main/resources/static/js/iframeSlidePage.js
@@ -0,0 +1,24 @@
+/**
+ * 頁面切換的iframe
+ * @returns true or false
+ */
+
+window.addEventListener('load', function() {
+	// 獲取 iframe 和按鈕元素
+	var iframe = document.querySelector('.ifr');
+	var navLinks = document.querySelectorAll('.nav-link');
+	
+	// 監聽所有的nav-link的點擊事件
+	navLinks.forEach(function(navLink) {
+	    navLink.addEventListener('click', function() {
+	        var dataValue = navLink.getAttribute('data-value');
+	        iframe.contentDocument.location.href = dataValue;
+	    });
+	});
+});
+
+$(document).ready(function(){
+    $('.sub-btn').click(function(){
+        $(this).next('.sub-menu').slideToggle();
+    });
+});  
